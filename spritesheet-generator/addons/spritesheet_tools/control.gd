3dda8a3b69f9680280a47ca147a806026146777f
Lichen Liu <liu.lichen63@gmail.com>
2024-12-24 07:36:04 +0800

    add file dialog

--- spritesheet-generator/addons/spritesheet_tools/control.gd
+++ spritesheet-generator/addons/spritesheet_tools/control.gd
@@ -1,5 +1,22 @@
 @tool
 extends Control
 
+var selected_files: PackedStringArray
+
+@onready var select_file_dialog: FileDialog = $SelectFileDialog
+@onready var spritesheet_preview: TextureRect = $SpritesheetPreview
+
 func _on_select_file_pressed() -> void:
-    pass
+    self.select_file_dialog.show()
+
+func _on_select_file_dialog_file_selected(path: String) -> void:
+    self.selected_files.push_back(path)
+
+func _on_select_file_dialog_files_selected(paths: PackedStringArray) -> void:
+    self.selected_files = paths
+
+func _on_select_file_dialog_confirmed() -> void:
+    pass # Replace with function body.
+
+func _on_select_file_dialog_canceled() -> void:
+    self.selected_files.clear()
--- spritesheet-generator/addons/spritesheet_tools/spritesheet_tools.gd
+++ spritesheet-generator/addons/spritesheet_tools/spritesheet_tools.gd
@@ -3,8 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-    var editor_file_dialog := EditorFileDialog.new()
-    editor_file_dialog.add_filter("*.jpg, *.jpeg, *.png", "Supported Images")
+    pass
 
 
 func _exit_tree() -> void:
