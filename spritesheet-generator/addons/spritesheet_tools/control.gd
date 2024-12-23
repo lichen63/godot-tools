c57fd2da5d3a33c1dc1da82343181e478e6f9abf
Lichen Liu <liu.lichen63@gmail.com>
2024-12-23 22:27:33 +0800

    setup main ui

---
+++ spritesheet-generator/addons/spritesheet_tools/control.gd
@@ -0,0 +1,5 @@
+@tool
+extends Control
+
+func _on_select_file_pressed() -> void:
+    pass
--- spritesheet-generator/addons/spritesheet_tools/spritesheet_tools.gd
+++ spritesheet-generator/addons/spritesheet_tools/spritesheet_tools.gd
@@ -3,8 +3,8 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-    # Initialization of the plugin goes here.
-    pass
+    var editor_file_dialog := EditorFileDialog.new()
+    editor_file_dialog.add_filter("*.jpg, *.jpeg, *.png", "Supported Images")
 
 
 func _exit_tree() -> void:
