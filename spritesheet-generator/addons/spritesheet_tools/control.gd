2652e7282c1494b15964e25e925ea97fe4cc85d0
Lichen Liu <liu.lichen63@gmail.com>
2024-12-25 07:07:22 +0800

    can show selected files, add sample images

--- spritesheet-generator/addons/spritesheet_tools/control.gd
+++ spritesheet-generator/addons/spritesheet_tools/control.gd
@@ -1,10 +1,14 @@
 @tool
 extends Control
 
-var selected_files: PackedStringArray
+var selected_files: PackedStringArray = PackedStringArray()
+var thumbnail_size: Vector2 = Vector2(64, 64)
+var rows: int = 2
+var columns: int = 3
+var margin: int = 10
 
 @onready var select_file_dialog: FileDialog = $SelectFileDialog
-@onready var spritesheet_preview: TextureRect = $SpritesheetPreview
+@onready var spritesheet_preview: Control = $SpritesheetPreview
 
 func _on_select_file_pressed() -> void:
     self.select_file_dialog.show()
@@ -16,7 +20,13 @@ func _on_select_file_dialog_files_selected(paths: PackedStringArray) -> void:
     self.selected_files = paths
 
 func _on_select_file_dialog_confirmed() -> void:
-    pass # Replace with function body.
+    for file_path in selected_files:
+        self.spritesheet_preview.add_child(self.load_image_as_texture(file_path))
 
 func _on_select_file_dialog_canceled() -> void:
     self.selected_files.clear()
+
+func load_image_as_texture(file_path: String) -> TextureRect:
+    var texture_rect := TextureRect.new()
+    texture_rect.texture = ImageTexture.create_from_image(Image.load_from_file(file_path))
+    return texture_rect
