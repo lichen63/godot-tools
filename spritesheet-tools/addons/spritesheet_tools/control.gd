e44d5ab091f931c9ad313f8fedf1bc0b6c783bc8
Lichen Liu <liu.lichen63@gmail.com>
2024-12-30 06:56:34 +0800

    feature: save all images at once

--- spritesheet-tools/addons/spritesheet_tools/control.gd
+++ spritesheet-tools/addons/spritesheet_tools/control.gd
@@ -1,9 +1,11 @@
 @tool
 extends Control
 
+const EXPORT_FILE_NAME_FORMAT = "export_%d.png"
+
 var selected_files: PackedStringArray = PackedStringArray()
-var preview_control_list: Array[Control] = []
-var preview_cur_control_index: int = 0
+var preview_viewport_list: Array[SubViewport] = []
+var preview_cur_viewport_index: int = 0
 
 @onready var select_file_dialog: FileDialog = $SelectFileDialog
 @onready var size_x_edit: LineEdit = $PropertyContainer/SpritesheetProperty/SizeProperty/SizeValue/X
@@ -12,7 +14,9 @@ var preview_cur_control_index: int = 0
 @onready var error_dialog: AcceptDialog = $ErrorDialog
 @onready var page_number: LineEdit = $PageSwitchContainer/Number
 @onready var save_path_edit: LineEdit = $PropertyContainer/SpritesheetProperty/SavePath
-@onready var preview_viewport: SubViewport = $Preview/PreviewViewportContainer/PreviewViewport
+@onready var preview_viewport_container: SubViewportContainer = $Preview/PreviewViewportContainer
+@onready var save_image_dialog: AcceptDialog = $SaveImageDialog
+@onready var save_image_container: SubViewportContainer = $SaveImageDialog/SubViewportContainer
 
 func _on_select_file_pressed() -> void:
     self.select_file_dialog.show()
@@ -31,36 +35,60 @@ func _on_select_file_dialog_canceled() -> void:
     self.selected_files.clear()
 
 func _on_page_left_pressed() -> void:
-    self.show_next_preview_control(false)
+    self.show_next_or_prev_viewport(false)
 
 func _on_page_right_pressed() -> void:
-    self.show_next_preview_control(true)
+    self.show_next_or_prev_viewport(true)
 
 func _on_save_to_file_pressed() -> void:
     var save_path_string: String = self.save_path_edit.text
     if not self.is_save_path_valid(save_path_string):
-        self.show_error_with_message("Invalid save path, it should begin with 'user://'")
+        self.show_error_with_message("Invalid save path.")
         return
-    var texture: ViewportTexture = self.preview_viewport.get_texture()
-    if not texture:
-        print("Failed to get texture")
-        return
-    var image: Image = texture.get_image()
-    if image:
-        var err = image.save_png(save_path_string)
-        if err == OK:
-            print("Image saved successfully at: ", save_path_string)
+    self.save_image_dialog.show()
+    for viewport in self.preview_viewport_list:
+        var file_path: String = self.get_next_savable_file_path(save_path_string)
+        if file_path.is_empty():
+            return
+        for child in self.save_image_container.get_children():
+            self.save_image_container.remove_child(child)
+            child.queue_free()
+        var dup_viewport = viewport.duplicate()
+        self.save_image_container.add_child(dup_viewport)
+        await self.get_tree().create_timer(1).timeout
+        var texture: ViewportTexture = dup_viewport.get_texture()
+        if not texture:
+            print("Failed to get texture")
+            return
+        var image: Image = texture.get_image()
+        if image:
+            var err = image.save_png(file_path)
+            if err == OK:
+                print("Image saved successfully at: ", file_path)
+            else:
+                print("Failed to save image, Error: ", err)
         else:
-            print("Failed to save image, Error: ", err)
-    else:
-        print("Failed to convert texture to image")
+            print("Failed to convert texture to image")
+
+func get_next_savable_file_path(save_path: String) -> String:
+    var index: int = 0
+    while index < 99999:
+        var file_path = save_path.path_join(EXPORT_FILE_NAME_FORMAT % index)
+        if not FileAccess.file_exists(file_path):
+            return file_path
+        index += 1
+    print("Cannot find savable file path")
+    return ""
 
 func is_save_path_valid(save_path: String) -> bool:
     if not save_path.begins_with("user://"):
         print("Save path must begin with 'user://'")
         return false
     if not ensure_directory_exists(save_path):
-        print("Failed to prepare the save directory.")
+        print("Failed to prepare the save directory")
+        return false
+    if not save_path.ends_with("/"):
+        print("Save path should end with '/'")
         return false
     return true
 
@@ -73,16 +101,16 @@ func ensure_directory_exists(save_path: String) -> bool:
             return false
     return true
 
-func show_next_preview_control(is_next: bool) -> void:
+func show_next_or_prev_viewport(is_next: bool) -> void:
     var next_index: int = -1
-    var control_size: int = self.preview_control_list.size()
+    var list_size: int = self.preview_viewport_list.size()
     if is_next:
-        next_index = self.preview_cur_control_index + 1 if self.preview_cur_control_index < control_size - 1 else 0
+        next_index = self.preview_cur_viewport_index + 1 if self.preview_cur_viewport_index < list_size - 1 else 0
     else:
-        next_index = self.preview_cur_control_index - 1 if self.preview_cur_control_index > 0 else control_size - 1
-    if next_index == self.preview_cur_control_index or next_index < 0 or next_index >= control_size:
+        next_index = self.preview_cur_viewport_index - 1 if self.preview_cur_viewport_index > 0 else list_size - 1
+    if next_index == self.preview_cur_viewport_index or next_index < 0 or next_index >= list_size:
         return
-    self.preview_cur_control_index = next_index
+    self.preview_cur_viewport_index = next_index
     self.update_preview_num_text()
     self.update_preview_image()
 
@@ -93,10 +121,14 @@ func load_image_as_texture(file_path: String) -> TextureRect:
     return texture_rect
 
 func clear_preview_images() -> void:
-    self.preview_control_list.clear()
-    self.preview_cur_control_index = 0
-    for child in self.preview_viewport.get_children():
-        self.preview_viewport.remove_child(child)
+    self.preview_viewport_list.clear()
+    self.preview_cur_viewport_index = 0
+    for viewport: SubViewport in self.preview_viewport_list:
+        for child in viewport.get_children():
+            viewport.remove_child(child)
+            child.queue_free()
+    for child in self.preview_viewport_container.get_children():
+        self.preview_viewport_container.remove_child(child)
         child.queue_free()
 
 func get_and_validate_input(input: LineEdit) -> int:
@@ -122,7 +154,8 @@ func show_images_on_preview(files: PackedStringArray) -> void:
         var texture_rect: TextureRect = self.load_image_as_texture(file_path)
         texture_list.append(texture_rect)
     var cur_index: int = 0
-    var cur_control: Control = Control.new()
+    var cur_viewport: SubViewport = SubViewport.new()
+    #cur_viewport.render_target_update_mode = SubViewport.UPDATE_ALWAYS
     while cur_index < texture_list.size():
         var texture_rect: TextureRect = texture_list[cur_index]
         var image_size: Vector2 = texture_rect.texture.get_size()
@@ -131,31 +164,31 @@ func show_images_on_preview(files: PackedStringArray) -> void:
             current_y += max_image_height_in_row + margin_value
             max_image_height_in_row = 0
         if current_y + image_size.y > max_size_y:
-            self.preview_control_list.append(cur_control)
-            cur_control.size = Vector2(max_size_x, current_y + max_image_height_in_row)
-            cur_control = Control.new()
+            self.preview_viewport_list.append(cur_viewport)
+            cur_viewport.size = Vector2(max_size_x, current_y + max_image_height_in_row)
+            cur_viewport = SubViewport.new()
+            #cur_viewport.render_target_update_mode = SubViewport.UPDATE_ALWAYS
             current_x = 0
             current_y = 0
             continue
         texture_rect.position = Vector2(current_x, current_y)
         current_x += image_size.x + margin_value
         max_image_height_in_row = max(max_image_height_in_row, image_size.y)
-        cur_control.add_child(texture_rect)
+        cur_viewport.add_child(texture_rect)
         cur_index += 1
-    cur_control.size = Vector2(max_size_x, current_y + max_image_height_in_row)
-    self.preview_control_list.append(cur_control)
-    print("Control size: [%d, %d]" % [cur_control.size.x, cur_control.size.y])
+    cur_viewport.size = Vector2(max_size_x, current_y + max_image_height_in_row)
+    self.preview_viewport_list.append(cur_viewport)
     self.update_preview_num_text()
     self.update_preview_image()
 
 func update_preview_num_text() -> void:
-    self.page_number.text = "%d/%d" % [self.preview_cur_control_index + 1, self.preview_control_list.size()]
+    self.page_number.text = "%d/%d" % [self.preview_cur_viewport_index + 1, self.preview_viewport_list.size()]
 
 func update_preview_image() -> void:
-    for child in self.preview_viewport.get_children():
-        self.preview_viewport.remove_child(child)
-    self.preview_viewport.size = self.preview_control_list[self.preview_cur_control_index].size
-    self.preview_viewport.add_child(self.preview_control_list[self.preview_cur_control_index])
+    for child in self.preview_viewport_container.get_children():
+        self.preview_viewport_container.remove_child(child)
+    self.preview_viewport_container.size = self.preview_viewport_list[self.preview_cur_viewport_index].size
+    self.preview_viewport_container.add_child(self.preview_viewport_list[self.preview_cur_viewport_index])
 
 func _on_test_1_pressed() -> void:
     self.clear_preview_images()
