4e1073518b3ee6a36e12b5a70b486757af8e83a8
Lichen Liu <liu.lichen63@gmail.com>
2025-01-26 08:02:11 +0800

    add command to menu item

--- spritesheet-tools/addons/spritesheet_tools/spritesheet_tools.gd
+++ spritesheet-tools/addons/spritesheet_tools/spritesheet_tools.gd
@@ -3,9 +3,13 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-    pass
+    add_tool_menu_item("Open Spritesheet Tools", run_generator)
+    get_editor_interface().get_command_palette().add_command("Open Spritesheet Tools", "addons/open_spritesheet_tools", run_generator)
 
 
 func _exit_tree() -> void:
-    # Clean-up of the plugin goes here.
-    pass
+    remove_tool_menu_item("Open Spritesheet Tools")
+    get_editor_interface().get_command_palette().remove_command("addons/open_spritesheet_tools")
+
+func run_generator() -> void:
+    get_editor_interface().play_custom_scene("res://addons/spritesheet_tools/control.tscn")
